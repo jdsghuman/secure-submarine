866dfc3e0ab16c6514d63195795079d939a0e944
JD Ghuman <jdsghuman@gmail.com>
2018-12-19 15:42:32 -0600

    Add secret levels

--- server/routes/secrets.router.js
+++ server/routes/secrets.router.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
+const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
-router.get('/', (req, res) => {
+router.get('/', rejectUnauthenticated, (req, res) => {
     console.log('req.user:', req.user);
-    pool.query('SELECT * FROM "secret";')
+    const queryString = `SELECT * FROM "secret" WHERE "secret".secrecy_level < $1;`;
+    const query = [req.user.clearance_level];
+    pool.query(queryString, query)
         .then(results => res.send(results.rows))
         .catch(error => {
             console.log('Error making SELECT for secrets:', error);
